6c1fd011d2c42ec26a43430bafd852796b801023
brandaistudio <brandai.studioo@gmail.com>
2025-10-27 02:38:33 +0100

    Create components/Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="p-4 text-center text-gray-500 dark:text-gray-400">
+      © 2025 NovaBrand. All rights reserved.
+    </footer>
+  );
+}
