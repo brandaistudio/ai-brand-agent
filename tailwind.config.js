4066b0207a64d1ecdf78d654ba8816d4bf0d0636
brandaistudio <brandai.studioo@gmail.com>
2025-10-27 01:10:39 +0100

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+styles/globals.css:
+
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
