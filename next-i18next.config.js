19558761f997fc9e158bafe44da71f76d6d616f6
brandaistudio <brandai.studioo@gmail.com>
2025-10-27 01:11:55 +0100

    Create next-i18next.config.js

---
+++ next-i18next.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ar'],
+  },
+};
+
+/public/locales/en.json:
+
+{
+  "welcome": "Welcome to NovaBrand",
+  "start": "Get Started",
+  "login": "Login",
+  "signup": "Sign Up"
+}
+
+/public/locales/ar.json:
+
+{
+  "welcome": "مرحبًا بك في NovaBrand",
+  "start": "ابدأ الآن",
+  "login": "تسجيل الدخول",
+  "signup": "إنشاء حساب"
+}
